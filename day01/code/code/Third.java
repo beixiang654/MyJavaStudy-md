aac5ca882ae772b431c74e007af42e8d64eb2618
beixiang <liuhao800218@gmail.com>
2023-06-08 19:38:05 +0800

    f

---
+++ day01/code/code/Third.java
@@ -0,0 +1,19 @@
+
+/**
+  *这个是一个类 是打印你好狗蛋的一个类
+  *文档注释
+  */
+class Third {
+	//main是程序的入口，少了它不行  单行注释
+
+	public static void main (String[] args) {
+		/*
+		dxsnmmns
+		cbdsndsn
+		上学呢你们
+		多行注释
+		
+		*/
+		System.out.println("你好,狗蛋");
+	}
+}
